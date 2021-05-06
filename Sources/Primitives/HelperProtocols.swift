47e95356dc7e6876bfdac4b6740bb3443ed35cc4
Ostap Danylovych <music.danos@gmail.com>
2021-05-06 20:32:52 +0300

    Removed ScaleFixedData from BlockNumberProtocol

--- Sources/Primitives/HelperProtocols.swift
+++ Sources/Primitives/HelperProtocols.swift
@@ -8,7 +8,7 @@
 import Foundation
 import ScaleCodec
 
-public protocol BlockNumberProtocol: ScaleFixedData, ScaleDynamicCodable, SDefault, Codable {
+public protocol BlockNumberProtocol: ScaleDynamicCodable, SDefault, Codable {
     static var firstBlock: Self { get }
 }
 
@@ -20,13 +20,7 @@ extension UnsignedInteger {
 //    public static var firstBlock: Self { 0 }
 //}
 
-extension UInt32: BlockNumberProtocol {}
-extension UInt64: BlockNumberProtocol {}
-//extension SUInt128: BlockNumberProtocol {}
-//extension SUInt256: BlockNumberProtocol {}
-//extension SUInt512: BlockNumberProtocol {}
-
-extension BlockNumberProtocol {
+extension UInt32: BlockNumberProtocol {
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         let data = try container.decode(Data.self)
@@ -44,3 +38,26 @@ extension BlockNumberProtocol {
         try container.encode(encode())
     }
 }
+
+extension UInt64: BlockNumberProtocol {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let data = try container.decode(Data.self)
+        guard data.count == Self.fixedBytesCount else {
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "Wrong data size \(data.count), expected \(Self.fixedBytesCount)"
+            )
+        }
+        try self.init(decoding: data)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(encode())
+    }
+}
+
+//extension SUInt128: BlockNumberProtocol {}
+//extension SUInt256: BlockNumberProtocol {}
+//extension SUInt512: BlockNumberProtocol {}
