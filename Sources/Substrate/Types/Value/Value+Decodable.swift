853d49078145589bda649674a7ddbee70300dfa4
Yehor Popovych <popovych.yegor@gmail.com>
2023-04-10 18:05:38 +0100

    partially implemented JSON decoding

--- Sources/Substrate/Types/Value/Value+Decodable.swift
+++ Sources/Substrate/Types/Value/Value+Decodable.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import ScaleCodec
 
 extension Value where C == RuntimeTypeId {
     public init(from decoder: Decoder, `as` type: RuntimeTypeId, registry: Registry) throws {
@@ -27,6 +28,14 @@ extension Value where C == RuntimeTypeId {
             self = try Self._decodeSequence(from: &container, type: type, valueType: vType, registry: registry)
         case .variant(variants: let vars):
             self = try Self._decodeVariant(from: &container, path: typeInfo.pathBasedName, type: type, variants: vars, registry: registry)
+        case .array(count: let count, of: let vType):
+            self = try Self._decodeArray(from: &container, type: type, count: count, valueType: vType, registry: registry)
+        case .tuple(components: let fields):
+            self = try Self._decodeTuple(from: &container, type: type, fields: fields, registry: registry)
+        case .primitive(is: let vType):
+            self = try Self._decodePrimitive(from: &container, type: type, prim: vType, registry: registry)
+        case .compact(of: let vType):
+            
         }
     }
 }
@@ -94,11 +103,19 @@ public enum ValueDecodingContainer {
         }
     }
     
-    mutating func nextKey(key: String) throws {
+    mutating func next(key: String) throws {
         switch self {
         case .keyed(_, let container):
             self = .keyed(AnyCodableCodingKey(key), container)
-        default: throw try newError("NextKey: not a keyed container")
+        default: throw try newError("next(key:): not a keyed container")
+        }
+    }
+    
+    func contains(key: String) throws -> Bool {
+        switch self {
+        case .keyed(_, let container):
+            return container.contains(AnyCodableCodingKey(key))
+        default: throw try newError("contains(key:): not a keyed container")
         }
     }
     
@@ -109,10 +126,10 @@ public enum ValueDecodingContainer {
         }
     }
     
-    func count() -> Int? {
+    func count() throws -> Int? {
         switch self {
         case .unkeyed(let container): return container.count
-        default: return nil
+        default: throw try newError("count: not an unkeyed container")
         }
     }
     
@@ -133,7 +150,8 @@ public enum ValueDecodingContainer {
 
 private extension Value where C == RuntimeTypeId {
     static func _decodeComposite(
-        from: inout ValueDecodingContainer, type: RuntimeTypeId, fields: [RuntimeTypeField], registry: Registry
+        from: inout ValueDecodingContainer, type: RuntimeTypeId,
+        fields: [RuntimeTypeField], registry: Registry
     ) throws -> Self {
         guard fields.count > 0 else {
             guard try from.decodeNil() else {
@@ -147,7 +165,7 @@ private extension Value where C == RuntimeTypeId {
             var value = try from.nestedKeyedContainer()
             var map: [String: Value<C>] = Dictionary(minimumCapacity: fields.count)
             for field in fields {
-                try value.nextKey(key: field.name!)
+                try value.next(key: field.name!)
                 map[field.name!] = try Value(from: &value,
                                              as: field.type,
                                              registry: registry)
@@ -163,7 +181,8 @@ private extension Value where C == RuntimeTypeId {
     }
     
     static func _decodeSequence(
-        from: inout ValueDecodingContainer, type: RuntimeTypeId, valueType: RuntimeTypeId, registry: Registry
+        from: inout ValueDecodingContainer, type: RuntimeTypeId,
+        valueType: RuntimeTypeId, registry: Registry
     ) throws -> Self {
         guard let vTypeInfo = registry.resolve(type: valueType) else {
             throw DecodingError.typeNotFound(valueType)
@@ -181,7 +200,7 @@ private extension Value where C == RuntimeTypeId {
         } else { // array
             var value = try from.nestedUnkeyedContainer()
             var values = Array<Self>()
-            if let count = value.count() {
+            if let count = try value.count() {
                 values.reserveCapacity(count)
             }
             while try !value.isAtEnd() {
@@ -195,41 +214,132 @@ private extension Value where C == RuntimeTypeId {
         from: inout ValueDecodingContainer, path: String?, type: RuntimeTypeId,
         variants: [RuntimeTypeVariantItem], registry: Registry
     ) throws -> Self {
-        guard path != "Option" else {
+        guard path != "Option" else { // Option<T> can be null or value
             let someType = variants.first(where: { $0.name == "Some" })!.fields[0].type
             if try from.decodeNil() {
                 return Value(value: .variant(.sequence(name: "None", values: [])), context: type)
             }
-            return try Self._decodeComposite(from: &from, type: someType, fields: <#T##[RuntimeTypeField]#>, registry: <#T##Registry#>)
+            let some = try Value(from: &from, as: someType, registry: registry)
+            return Value(value: .variant(.sequence(name: "Some", values: [some])), context: type)
         }
         if let data = try? from.decode(Data.self) { // SCALE serialized
             return try Value(from: registry.decoder(with: data), as: type, registry: registry)
         } else {
-            
+            var container = try from.nestedKeyedContainer()
+            var variant: RuntimeTypeVariantItem
+            if try container.contains(key: "name") {
+                try container.next(key: "name")
+                let name = try container.decode(String.self)
+                guard let found = variants.first(where: { $0.name == name }) else {
+                    throw try container.newError("Found unknown variant: \(name)")
+                }
+                try container.next(key: "values")
+                variant = found
+            } else if try container.contains(key: "type") {
+                try container.next(key: "type")
+                let name = try container.decode(String.self)
+                guard let found = variants.first(where: { $0.name == name }) else {
+                    throw try container.newError("Found unknown variant: \(name)")
+                }
+                container = from
+                variant = found
+            } else {
+                let found = try variants.reduce(nil) { prev, cur in
+                    try prev ?? (container.contains(key: cur.name) ? cur : nil)
+                }
+                guard let found = found else {
+                    throw try container.newError("variant not found")
+                }
+                try container.next(key: found.name)
+                variant = found
+            }
+            let value = try Self._decodeComposite(from: &container, type: type,
+                                                  fields: variant.fields, registry: registry)
+            switch value.value {
+            case .map(let map):
+                return Value(value: .variant(.map(name: variant.name, fields: map)), context: type)
+            case .sequence(let fields):
+                return Value(value: .variant(.sequence(name: variant.name, values: fields)), context: type)
+            default: fatalError("Should never be called for composite")
+            }
         }
-//        let index = try decoder.decode(.enumCaseId)
-//        guard let variant = variants.first(where: { $0.index == index }) else {
-//            throw DecodingError.variantNotFound(index, type)
-//        }
-//        let composite = try _decodeComposite(from: decoder, type: type, fields: variant.fields, registry: registry)
-//        if let map = composite.map {
-//            return Value(value: .variant(.map(name: variant.name, fields: map)), context: type)
-//        }
-//        return Value(value: .variant(.sequence(name: variant.name, values: composite.sequence!)), context: type)
     }
     
-    static func _decodeOption(
-        from: inout ValueDecodingContainer, type: RuntimeTypeId, registry: Registry
+    static func _decodeArray(
+        from: inout ValueDecodingContainer, type: RuntimeTypeId,
+        count: UInt32, valueType: RuntimeTypeId, registry: Registry
     ) throws -> Self {
-        
-        let index = try decoder.decode(.enumCaseId)
-        guard let variant = variants.first(where: { $0.index == index }) else {
-            throw DecodingError.variantNotFound(index, type)
+        guard let vTypeInfo = registry.resolve(type: valueType) else {
+            throw DecodingError.typeNotFound(valueType)
         }
-        let composite = try _decodeComposite(from: decoder, type: type, fields: variant.fields, registry: registry)
-        if let map = composite.map {
-            return Value(value: .variant(.map(name: variant.name, fields: map)), context: type)
+        if case .primitive(is: .u8) = vTypeInfo.definition { // [u8] array
+            if let data = try? from.decode(Data.self) {
+                guard data.count == count else {
+                    throw try from.newError("Wrong data size: \(data.count), expected: \(count)")
+                }
+                return Value(value: .primitive(.bytes(data)), context: type)
+            } else if let data = try? from.decode([UInt8].self) {
+                guard data.count == count else {
+                    throw try from.newError("Wrong array size: \(data.count), expected: \(count)")
+                }
+                return Value(value: .primitive(.bytes(Data(data))), context: type)
+            } else {
+                throw try from.newError("Expected hex or [u8] for data")
+            }
+        } else if let data = try? from.decode(Data.self) { // SCALE serialized
+            return try Value(from: registry.decoder(with: data), as: type, registry: registry)
+        } else { // array
+            var value = try from.nestedUnkeyedContainer()
+            var values = Array<Self>()
+            values.reserveCapacity(Int(count))
+            while try !value.isAtEnd() {
+                values.append(try Value(from: &value, as: valueType, registry: registry))
+            }
+            guard values.count == count else {
+                throw try from.newError("Wrong array size: \(values.count), expected: \(count)")
+            }
+            return Value(value: .sequence(values), context: type)
         }
-        return Value(value: .variant(.sequence(name: variant.name, values: composite.sequence!)), context: type)
+    }
+    
+    static func _decodeTuple(
+        from: inout ValueDecodingContainer, type: RuntimeTypeId, fields: [RuntimeTypeId], registry: Registry
+    ) throws -> Self {
+        // Should we check 1 element tuples as value?
+        if let data = try? from.decode(Data.self) { // SCALE serialized
+            return try Value(from: registry.decoder(with: data), as: type, registry: registry)
+        } else {
+            var container = try from.nestedUnkeyedContainer()
+            let seq = try fields.map { try Value(from: &container, as: $0, registry: registry) }
+            return Value(value: .sequence(seq), context: type)
+        }
+    }
+    
+    static func _decodePrimitive(
+        from: inout ValueDecodingContainer, type: RuntimeTypeId, prim: RuntimeTypePrimitive, registry: Registry
+    ) throws -> Self {
+        switch prim {
+        case .bool: return Value(value: .primitive(.bool(try from.decode(Bool.self))), context: type)
+        case .char:
+            let string = try from.decode(String.self)
+            guard string.unicodeScalars.count == 1 else {
+                throw try from.newError("Bad character string: \(string)")
+            }
+            return Value(value: .primitive(.char(Character(string.unicodeScalars.first!))), context: type)
+        case .str: return Value(value: .primitive(.string(try from.decode(String.self))), context: type)
+        case .u8, .u16, .u32, .u64:
+            let value = try from.decode(HexOrNumber<UInt64>.self)
+            return Value(value: .primitive(.u256(UInt256(value.value))), context: type)
+        case .u128, .u256:
+            if let value = try? from.decode(UInt64.self) {
+                return Value(value: .primitive(.u256(UInt256(value))), context: type)
+            } else {
+                return Value(value: .primitive(.u256(try from.decode(UIntHex<UInt256>.self).value)),
+                             context: type)
+            }
+        case .i8, .i16, .i32, .i64, .i128, .i256:
+            return Value(value: .primitive(.i256(Int256(try from.decode(Int64.self)))),
+                         context: type)
+        }
     }
 }
