dff049430598686f918c37d7f805532de145d52b
Yehor Popovych <popovych.yegor@gmail.com>
2023-07-15 18:34:40 +0100

    metadata 14 is not fully supported

--- Tests/SubstrateTests/DynamicTypesLookupTests.swift
+++ Tests/SubstrateTests/DynamicTypesLookupTests.swift
@@ -11,24 +11,26 @@ import ScaleCodec
 
 final class DynamicTypesLookupTests: XCTestCase {
     func testMetadataV14() throws {
-        try metadataTest(data: Resources.inst.metadadav14())
+        try metadataTest(data: Resources.inst.metadadav14(), is14: true)
     }
     
     func testMetadataV15() throws {
-        try metadataTest(data: Resources.inst.metadadav15())
+        try metadataTest(data: Resources.inst.metadadav15(), is14: false)
     }
     
-    func metadataTest(data: Data) throws {
+    func metadataTest(data: Data, is14: Bool) throws {
         let opaq = try ScaleCodec.decode(Optional<OpaqueMetadata>.self, from: data)!
         let metadata = try ScaleCodec.decode(VersionedMetadata.self, from: opaq.raw).metadata.asMetadata()
         let config = try DynamicConfig()
         
         let _ = try config.hasher(metadata: metadata)
-        let _ = try config.blockType(metadata: metadata)
+        if !is14 {
+            let _ = try config.blockType(metadata: metadata)
+            let _ = try config.feeDetailsType(metadata: metadata)
+            let _ = try config.transactionValidityErrorType(metadata: metadata)
+        }
         let _ = try config.dispatchInfoType(metadata: metadata)
-        let _ = try config.feeDetailsType(metadata: metadata)
         let _ = try config.dispatchErrorType(metadata: metadata)
-        let _ = try config.transactionValidityErrorType(metadata: metadata)
         let _ = try config.extrinsicTypes(metadata: metadata)
         let _ = try config.eventType(metadata: metadata)
     }
